9376cdf261e17c40038dd7cfc29cc412a188de6e
FerranGaldon10 <ferrangaldon100@gmi.com>
2023-06-12 20:09:20 +0200

    Prueba3

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 // const getProducts = require("./getProducts");
-const getProductsCart = require("./GetProductsCart");
-const AddProductCart = require("./AddProductCart");
-const putProduct = require("./putProduct");
-const deleteProduct = require("./DeleteProduct");
+// const getProductsCart = require("./GetProductsCart");
+// const AddProductCart = require("./AddProductCart");
+// const putProduct = require("./putProduct");
+// const deleteProduct = require("./DeleteProduct");
 const getProducts = require("../controllers/getProducts");
+const getProductsCart = require("../controllers/GetProductsCart");
+const AddProductCart = require("../controllers/AddProductCart");
+const putProduct = require("../controllers/putProduct");
+const deleteProduct = require("../controllers/DeleteProduct");
 
 
 module.exports = {
