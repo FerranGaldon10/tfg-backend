1cd416556541016423544c6aae753c2660f65563
FerranGaldon10 <ferrangaldon100@gmi.com>
2023-06-20 14:57:31 +0200

    Prueba19

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended:true}));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
